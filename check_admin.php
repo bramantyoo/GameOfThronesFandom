07806293d1eb1ca2ddcc34554cafa709f69b83a7
andinur2203 <113890702+andinur2203@users.noreply.github.com>
2022-11-10 09:30:07 +0800

    Add files via upload

---
+++ check_admin.php
@@ -0,0 +1,29 @@
+<?php
+
+$users = [
+    "admin" => "12345"
+];
+
+if( empty(session_id()) && !headers_sent()){
+    session_start();
+}
+
+if (isset($_POST['user']) && !isset($_SESSION['user'])) {
+    if ($users[$_POST['user']] == $_POST['password']) {
+        $_SESSION['user'] = $_POST['user'];
+    }
+    if(!isset($_SESSION['user'])) {$failed = true;}
+}
+
+if (isset($_SESSION['user'])){
+    echo "
+        <script>
+        alert('Selamat Datang Admin');
+        document.location.href = 'index.php';
+        </script>";
+    header("Location:admin_page.php");
+} else {
+    echo "";
+}
+
+?>
