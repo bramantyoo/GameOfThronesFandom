784edac298c2847c4e254b377a7833168d556cd5
Adlinassp <93045326+Adlinassp@users.noreply.github.com>
2022-11-14 08:21:35 +0800

    Update hapus_char.php

---
+++ hapus_char.php
@@ -0,0 +1,14 @@
+<?php 
+
+require 'config.php';
+
+if(isset($_GET['id_char'])){
+    $id_char = $_GET['id_char'];
+
+    $result = mysqli_query($db, 
+        "DELETE FROM karakter WHERE id_char='$id_char'");
+
+    if($result){
+        header("Location:character.php");
+    }
+}
