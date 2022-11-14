5c6447a9c132abdec18d376428933fb02df07239
bramantyoo <117274080+bramantyoo@users.noreply.github.com>
2022-11-14 23:47:41 +0800

    Update user_page.php
    final user page

--- user_page.php
+++ user_page.php
@@ -30,7 +30,7 @@ Game Of Thrones Fandom Final Project
   <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@600&display=swap" rel="stylesheet">
 
   <!-- STYLESHEET -->
-  <link rel="stylesheet" href="./user.css">
+  <link rel="stylesheet" href="css/user_page.css">
 
 </head>
 
@@ -40,60 +40,38 @@ Game Of Thrones Fandom Final Project
   <header id="header">
     <div class="topnav" id="myTopnav">
       <a href="#"><img class="logo animate" src="images/logoheader.png" alt=""></a>
-      <a id="active"></a>
-      <a href="#">Home</a>
-      <a href="#background">Character</a>
-      <a href="#mission">House</a>
-      <a href="#about">About</a>
-      <a href="#testimonial">Quote</a>
+      <a href="user_page.php">Home</a>
+      <a href="char_user.php">Character</a>
+      <a href="house_user.php">House</a>
+      <a href="">About</a>
       <a href="#form" class="btn2" style="float:right;"><i class="fa fa-handshake-o"></i> Logout</a>
       <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="NavBar()">&#9776;</a>
     </div>
   </header>
   <div class="header">
+
     <!--BANNER-->
     <div class="banner">
       <section>
-          <img src="images/gotlogo.png" alt="">
-          <!-- <h1 class="title">GAME OF THRONES FANDOM</h1> -->
-          <h4 class="sub-title">Unofficial Fandom site of Game of Thrones. </h4>
-          <!-- background music -->
-          <center>
+        <img src="asset/image/gotlogo.png" alt="">
+        <!-- <h1 class="title">GAME OF THRONES FANDOM</h1> -->
+        <h4 class="sub-title">Unofficial Fandom site of Game of Thrones. </h4>
+        <!-- background music -->
+        <center>
           <audio autoplay loop controls volume="0.5">
-          <source src="music/Ramin_Djawadi_-_A_Song_of_Ice_and_Fire.mp3" type="audio/mp3"/>
+            <source src="music/Ramin_Djawadi_-_A_Song_of_Ice_and_Fire.mp3" type="audio/mp3" />
           </audio>
-          </center>
-          <!-- end background music -->
+        </center>
+        <!-- end background music -->
       </section>
     </div>
   </div>
-
-  <!--CARDS-->
-  <!-- <div class="section1">
-<section class="meta">
-Numbers will make a difference, we need participation from Debt Counsellors to proceed with a class action lawsuit, no cost to debt counsellors.
-</section>
-<section class="cards">
-<div class="card">
-<img src="https://i.ibb.co/r0H1j4k/01.png" alt="" class="icons">
-<h3>No need to appear in court</h3>
-</div>
-<div class="card">
-<img src="https://i.ibb.co/sjqc0gs/02.png" alt="" class="icons">
-<h3>No cost</h3>
-</div>
-<div class="card">
-<img src="https://i.ibb.co/nmGRKKS/03.png" alt="" class="icons">
-<h3>We will do the legal work</h3>
-</div>
-</section>
-</div> -->
 
   <!--CONTAINER-->
   <div class="container">
     <div class="section2 sections" id="about">
       <section>
-        <img src="images/ironthronecrop2.png">
+        <img src="asset/image/ironthronecrop2.png">
       </section>
       <section>
         <em>Prologue</em>
@@ -104,163 +82,35 @@ Numbers will make a difference, we need participation from Debt Counsellors to p
       </section>
     </div>
 
-    <!-- QUotes -->
-    <div class="testimonial-section" id="testimonial">
-      <div class="container">
-        <div class="testimonial-header">
-          <h3><img src="images/icon3.png">Best<span>Quotes</span></h3>
-          <p>Best quotes in Game of Thrones.</p>
+    <!--FOOTER-->
+    <footer>
+      <div class="footer">
+        <div class="row">
+          <a href="instagram.com/an_atra22"><i class="fa fa-instagram"></i></a>
+          <a href="instagram.com/adlnshafa"><i class="fa fa-instagram"></i></a>
+          <a href="instagram.com/bramantyoardi_"><i class="fa fa-instagram"></i></a>
         </div>
-        <div class="wmuSlider example1">
-          <div class="container-flueid">
-            <article style="position: absolute;">
-              <div class=" cont span_2_of_3  client-main">
-                <div class="slide-text">
-                  <h4> “Why is it that when one man builds a wall, the next man immediately needs to know what's on the other side?”</h4>
-                  <img src="images/icon8.png">
-                </div>
-              </div>
-            </article>
-            <article style="position: absolute;">
-              <div class=" cont span_2_of_3  client-main">
-                <div class="slide-text">
-                  <h4> "Once you've accepted your flaws, no one can use them against you."</h4>
-                  <img src="images/icon8.png">
-                </div>
-              </div>
-            </article>
-            <article style="position: absolute;">
-              <div class="cont span_2_of_3  client-main">
-                <div class="slide-text">
-                  <h4> "Tears aren't a woman's only weapon. The best one's between your legs."</h4>
-                  <img src="images/icon9.png">
-                </div>
-              </div>
-            </article>
-            <article style="position: absolute;">
-              <div class="cont span_2_of_3  client-main">
-                <div class="slide-text">
-                  <h4> "I am not questioning your honor, I am denying its existence."</h4>
-                  <img src="images/icon8.png">
-                </div>
-              </div>
-            </article>
-            <article style="position: absolute;">
-              <div class="cont span_2_of_3  client-main">
-                <div class="slide-text">
-                  <h4> "You are as big a fool as every other man. That little worm between your legs does half your thinking."</h4>
-                  <img src="images/icon9.png">
-                </div>
-              </div>
-            </article>
 
-            <script src="js/jquery.wmuSlider.js"></script>
-            <script>
-              $('.example1').wmuSlider();
-            </script>
-            <div class="clearfix"> </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="sections section3" id="mission">
-      <section>
-        <h1 class="title">OUR MISSION</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>We invite <b>existing</b> and <b>previous</b> debt counsellors to join our petition and bring much needed change!</p>
-      </section>
-    </div>
-
-    <!-- <div class="sections section4">
-      <section>
-        <span>
-          <p>What the petition is about and what laws and guidelines we want to change</p>
+        <div class="row">
           <ul>
-            <li>Fees and Fee guidelines</li>
-            <li>Time frame (60-days)</li>
-            <li>Differentiating between NCT legal work and Magistrate Legal Work</li>
-            <li>Application date versus date client is placed under debt review</li>
+            <li><a href="user_page.php">Home</a></li>
+            <li><a href="char_user.php">Character</a></li>
+            <li><a href="house_char.php">House</a></li>
+            <li><a href="form_contact.php">Feedback for us</a></li>
           </ul>
-        </span>
-      </section>
-      <section>
-        <h1 class="title">Join the Petition For Declaratory Relief</h1>
-        <div id="form">
-          <form action="#" method="POST">
-            <span class="half-col">
-              <fieldset>
-                <input type="text" name="fname" placeholder="Fullname*" required="" maxlenght="60" />
-              </fieldset>
-              <fieldset>
-                <input type="text" name="sname" placeholder="Surname*" required="" maxlenght="60" />
-              </fieldset>
-            </span>
-            <span class="half-col">
-              <fieldset>
-                <input type="tel" name="phone" placeholder="Contact no*" required="" maxlenght="15" />
-              </fieldset>
-              <fieldset>
-                <input type="email" name="email" placeholder="Email*" required="" maxlenght="100" />
-              </fieldset>
-            </span>
-            <span class="full-col">
-              <fieldset>
-                <input type="text" name="regno" placeholder="NCRDR Reg no*" required="" maxlenght="100" />
-              </fieldset>
-            </span>
-            <span class="full-col">
-              <fieldset>
-                <input type="text" name="regstatus" placeholder="NCRDR Reg status*" required="" maxlenght="100" />
-              </fieldset>
-            </span>
-            <span class="full-col">
-              <fieldset>
-                <input type="text" name="cname" placeholder="Company name that you represent*" required="" maxlenght="150" />
-              </fieldset>
-            </span>
-            <span class="full-col">
-              <fieldset>
-                <button class="btn1" type="submit">SUBMIT</button>
-              </fieldset>
-            </span>
-          </form>
         </div>
-      </section>
-    </div>
-  </div> -->
 
-  <!--FOOTER-->
-  <footer>
-    <div class="footer">
-      <div class="row">
-        <a href="#"><i class="fa fa-facebook"></i></a>
-        <a href="#"><i class="fa fa-instagram"></i></a>
-        <a href="#"><i class="fa fa-youtube"></i></a>
-        <a href="#"><i class="fa fa-twitter"></i></a>
+        <div class="row">
+          Copyright © 2022 Group 2 (Web Programming) - All rights reserved
+        </div>
       </div>
+    </footer>
 
-      <div class="row">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Character</a></li>
-          <li><a href="#">House</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">Terms & Conditions</a></li>
-        </ul>
-      </div>
+    <!--ADDITIONAL-->
+    <a href="#" id="roll_back" class="animate"><i class="fa fa-angle-up"></i></a>
 
-      <div class="row">
-        Copyright © 2022 Group 2 (Web Programming) - All rights reserved
-      </div>
-    </div>
-  </footer>
-
-  <!--ADDITIONAL-->
-  <a href="#" id="roll_back" class="animate"><i class="fa fa-angle-up"></i></a>
-
-  <!-- JAVASCRIPT -->
-  <script type="text/javascript" src="./user.js"></script>
+    <!-- JAVASCRIPT -->
+    <script type="text/javascript" src="js/user_page.js"></script>
 </body>
+
 </html>
